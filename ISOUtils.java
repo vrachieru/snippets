cd4d4cdce18a459d72c287cb8ee4a1c9372dc102
Victor Rachieru <vick91r@gmail.com>
2015-10-12 10:52:50 +0300

    [java] Add ISOUtils

---
+++ ISOUtils.java
@@ -0,0 +1,16 @@
+public final class ISOUtils {
+  private static final Set<String> ISO_LANGUAGES = new HashSet<String>
+    (Arrays.asList(Locale.getISOLanguages()));
+  private static final Set<String> ISO_COUNTRIES = new HashSet<String>
+    (Arrays.asList(Locale.getISOCountries()));
+
+  private ISOUtils() {}
+
+  public static boolean isValidISOLanguage(String languageCode) {
+    return ISO_LANGUAGES.contains(languageCode);
+  }
+
+  public static boolean isValidISOCountry(String countryCode) {
+    return ISO_COUNTRIES.contains(countryCode);
+  }
+}
